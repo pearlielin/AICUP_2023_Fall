dce81f8cfc82e6ddb1e4141625393454fbcb40c6
pearlielin <pearlie.lin10@gmail.com>
2023-12-08 19:34:45 +0800

    modify ensemble

--- ensemble_models_results.py
+++ ensemble_models_results.py
@@ -4,12 +4,12 @@ import pandas as pd
 if __name__ == '__main__':
     col_names = ['id', 'phi_type', 'start_pos',
                  'end_pos', 'value', 'normalized_value']
-    model_a_answer = pd.read_csv(
-        'model_a_result.txt', sep="\t", names=col_names)
-    model_A_type = ['IDNUM', 'MEDICALRECORD', 'PATIENT', 'STREET', 'URL',
+    roberta_answer = pd.read_csv(
+        'roberta_answer.txt', sep="\t", names=col_names)
+    roberta_type = ['IDNUM', 'MEDICALRECORD', 'PATIENT', 'STREET', 'URL',
                     'CITY', 'ZIP', 'HOSPITAL', 'DOCTOR', 'PHONE', 'AGE']
-    model_a_answer = model_a_answer[model_a_answer['phi_type'].isin(
-        model_A_type)]
+    roberta_answer = roberta_answer[roberta_answer['phi_type'].isin(
+        roberta_type)]
 
     llama2_answer = pd.read_csv(
         'llama2_answer.txt', sep="\t", names=col_names)
@@ -18,5 +18,5 @@ if __name__ == '__main__':
     llama2_answer = llama2_answer[llama2_answer['phi_type'].isin(
         llama2_type)]
 
-    results = pd.concat([model_a_answer, llama2_answer])
+    results = pd.concat([roberta_answer, llama2_answer])
     results.to_csv('answer.txt', sep='\t', index=False, header=False)
